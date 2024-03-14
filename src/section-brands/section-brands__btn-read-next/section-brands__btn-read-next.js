4dbcb30ae9b18b1d138e63644c68690f028e8044
yakutenko.d.s <yakutenko.d.s@astra-best.ru>
2024-03-14 13:20:24 +0400

    fixed read-next-btn text

--- src/section-brands/section-brands__btn-read-next/section-brands__btn-read-next.js
+++ src/section-brands/section-brands__btn-read-next/section-brands__btn-read-next.js
@@ -6,7 +6,7 @@ const openCloseCarousel = () => {
 
     let brandsCarousel = document.querySelector('.swiper-wrapper'); 
     brandsCarousel.style.height = clickCounter % 2 ? '160px' : 'fit-content';
-    btnRN.innerText = clickCounter % 2 ? 'Скрыть' : 'Показать все'; ;
+    btnRN.innerText = clickCounter % 2 ? 'Показать все' : 'Скрыть';
 }
 
 btnRN.addEventListener('click', openCloseCarousel);
