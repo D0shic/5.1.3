016a558867a75419f28c2e1c945c2b2850f1938c
yakutenko.d.s <yakutenko.d.s@astra-best.ru>
2024-03-18 11:14:31 +0400

    test

--- src/swiper/swiper.js
+++ src/swiper/swiper.js
@@ -1,38 +1,38 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+  const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
+    let swiper;
 
-    const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
-      let swiper;
-  
-      breakpoint = window.matchMedia(breakpoint);
-  
-      const enableSwiper = function(className, settings) {
-        swiper = new Swiper(className, settings);
-  
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function (className, settings) {
+      swiper = new Swiper(className, settings);
+    };
+
+    const checker = function () {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
       }
-  
-      const checker = function() {
-        if (breakpoint.matches) {
-          return enableSwiper(swiperClass, swiperSettings);
-        } else {
-          if (swiper !== undefined) swiper.destroy(true, true);
-          return;
-        }
-      };
-  
-      breakpoint.addEventListener('change', checker);
-      checker();
-    }
-  
-    resizableSwiper(
-      '(max-width: 767px)',
-      '.swiper',
-      {
-        speed: 500,
-        slidesPerView: 1.3,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-      },
-    );
+    };
+
+    breakpoint.addEventListener("change", checker);
+    checker();
+  };
+
+  resizableSwiper("(max-width: 767px)", ".swiper", {
+    slidesPerView: "auto",
+    loop: true,
+    speed: 5000,
+    autoplay: {
+      enabled: true,
+      delay: 1,
+    },
+    slidesPerView: 1.3,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
+});
